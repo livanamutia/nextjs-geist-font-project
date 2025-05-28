47b872fb84ae88356cfe6b7542cd1a488839baf3
livanamutia <livanamutia@gmail.com>
2025-05-28 22:08:28 +0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1748444906117

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_landing_aa27ba24.css",
+  "static/chunks/_7493323a._.js"
+],
+    source: "dynamic"
+});
